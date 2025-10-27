ca2cb9691d75fac81a697a3fe3adbf5eaec69778
Sean Roberts <sean.roberts@netlify.com>
2025-10-27 13:22:00 -0400

    improving azure oauth clients

--- netlify/functions/oauth-server.ts
+++ netlify/functions/oauth-server.ts
@@ -19,17 +19,31 @@ const staticClients: ClientMetadata[] = [
   // Point of contacts for Azure AI Foundry
   // zhuoqunli@microsoft.com 
   // AzureToolsCatalog@microsoft.com
+
+  // oauth app name "Azure AI Foundry"
   {
-    client_id: 'azure-ai-foundry',
+    client_id: 'yncg92fdmoCfvrPSIbNH9ihx9oI5iFFoKqTY7sVQkEA',
     client_secret: process.env.CLIENT_SECRET_AZURE_AI_FOUNDRY || 'supersecret!!!!!321aasdf23123cdfdSDFSKL;;;8', // Use secure secrets in production
     redirect_uris: [
-      'https://global.consent.azure-apim.net/redirect/foundrynetlifymcp',
+      'https://global.consent.azure-apim.net/redirect/foundrynetlifymcp'
+    ],
+    grant_types: ['authorization_code', 'refresh_token'],
+    response_types: ['code'],
+    token_endpoint_auth_method: 'client_secret_post',
+  },
+
+  // oauth app name "Azure AI Foundry Testing"
+  {
+    client_id: 'BHsAsy2hsx4NLRthhSVAA2IQ0W7d72H8o2fevaVqyaE',
+    client_secret: process.env.CLIENT_SECRET_AZURE_AI_FOUNDRY_TESTING || 'supersecret!!!!!321aasdf23123cdfdSDFSKL;;;8', // Use secure secrets in production
+    redirect_uris: [
       'https://global-test.consent.azure-apim.net/redirect/foundrynetlifymcp'
     ],
     grant_types: ['authorization_code', 'refresh_token'],
     response_types: ['code'],
     token_endpoint_auth_method: 'client_secret_post',
   },
+
   // Add more static clients as needed
 ];
 
